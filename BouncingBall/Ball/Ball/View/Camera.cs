052399269868542a0f85a738eb4d7028c21448ca
ms223uh@student.lnu.se <ms223uh@student.lnu.se>
2015-11-17 12:04:56 +0100

    Begun @ cameraClass
    - Added variables
    - Set the cordinations/scale

--- BouncingBall/Ball/Ball/View/Camera.cs
+++ BouncingBall/Ball/Ball/View/Camera.cs
@@ -10,5 +10,28 @@ namespace Ball.View
 {
     class Camera
     {
+        private float cordX;
+        private float cordY;
+        private float borderSize = 40;
+
+        public Camera(Viewport viewport)
+        {
+            cordX = viewport.Height;
+            cordY = viewport.Width;
+
+            if (cordY < cordX)
+            {
+                cordX = cordY;
+            }
+            else if(cordX < cordY)
+            {
+                cordY = cordX;
+            }
+
+            cordX = cordX - borderSize * 2;
+            cordY = cordY - borderSize * 2;
+        }
+
+
     }
 }
