287ffe8ed61d115bf71d0358c658ea31604d7906
ms223uh@student.lnu.se <ms223uh@student.lnu.se>
2015-11-18 12:34:44 +0100

    Done with ballViewClass
    - Added fields
    - Added functions to draw the playArea & call the simulation for the
    ball in playArea

--- BouncingBall/Ball/Ball/View/BallView.cs
+++ BouncingBall/Ball/Ball/View/BallView.cs
@@ -6,10 +6,43 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace Ball.View
+namespace Ball.View 
 {
     class BallView
     {
+        Model.BallSimulation ballSimulation;
+        Camera camera;
+        Texture2D ballTexture;
+        Texture2D gameBox;
+
+        public BallView(Model.BallSimulation ballSim, Camera viewCamera, GraphicsDevice graphics, Texture2D psyBall)
+        {
+            ballSimulation = ballSim;
+            camera = viewCamera;
+            ballTexture = psyBall;
+
+            gameBox = new Texture2D(graphics, 1, 1);
+            gameBox.SetData(new[] { Color.OrangeRed });
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            float psyBallScale = camera.ballScale(ballSimulation.simBallRadius(), ballTexture.Bounds.Width);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(gameBox, camera.gameArea(), Color.OrangeRed);
+            spriteBatch.Draw(ballTexture,
+                             camera.screenCord(ballSimulation.simBallPosition()),
+                             ballTexture.Bounds,
+                             Color.White, 0,
+                             new Vector2(ballTexture.Bounds.Width / 2, ballTexture.Height / 2),
+                             psyBallScale, SpriteEffects.None, 0
+                );
+
+            spriteBatch.End();
+        }
 
     }
 }
