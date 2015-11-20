c21ac59bab43a4c492dda9a09873068cb5c04e27
ms223uh@student.lnu.se <ms223uh@student.lnu.se>
2015-11-20 15:25:34 +0100

    GameView Update
    - Added fields
    - Added construct

---
+++ SplitterAndGravity/Game1/Game1/View/GameView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1.View
+{
+    class GameView
+    {
+
+        SpriteBatch spriteBatch;
+        SplitterSystem particle;
+        Camera camera;
+        Texture2D particleTexture;
+        float durationTime;
+
+
+        public GameView(SpriteBatch spriteBatch, Texture2D particleTexture, Camera camera)
+        {
+            this.spriteBatch = spriteBatch;
+            this.particleTexture = particleTexture;
+            this.camera = camera;
+            durationTime = 0;
+        }
+
+
+    }
+}
