ee2291ccb02b8c37b4fdb204a82993f54f61da3d
ms223uh@student.lnu.se <ms223uh@student.lnu.se>
2015-11-20 14:41:50 +0100

    Code in SplitterParticle
    - Some code in class
    - Have to do CameraClass first

--- SplitterAndGravity/Game1/Game1/View/SplitterParticle.cs
+++ SplitterAndGravity/Game1/Game1/View/SplitterParticle.cs
@@ -10,5 +10,30 @@ namespace Game1.View
 {
     class SplitterParticle
     {
+
+        private int seed;
+        private Vector2 startPostion;
+        private Vector2 particlePostion;
+        private Vector2 velocity;
+
+
+        public SplitterParticle(int seed, Vector2 startPositon)
+        {
+            this.seed = seed;
+            this.startPostion = startPostion;
+            this.particlePostion = new Vector2(startPositon.X, startPositon.Y);
+            velocity = new Vector2(1, 0);
+        }
+
+        internal void Update(float elapsedTimeSeconds)
+        {
+            particlePostion = particlePostion + velocity * elapsedTimeSeconds;
+        }
+
+        internal void Draw(SpriteBatch spriteBatch, Camera camera, Texture2D particleTexture)
+        {
+
+        }
+
     }
 }
