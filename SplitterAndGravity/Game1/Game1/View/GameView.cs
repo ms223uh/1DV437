eb6c67671b0d88d28b96d09bd4df439e1de1d238
ms223uh@student.lnu.se <ms223uh@student.lnu.se>
2015-11-20 17:10:27 +0100

    gameViewClass Done

--- SplitterAndGravity/Game1/Game1/View/GameView.cs
+++ SplitterAndGravity/Game1/Game1/View/GameView.cs
@@ -24,6 +24,33 @@ namespace Game1.View
             this.particleTexture = particleTexture;
             this.camera = camera;
             durationTime = 0;
+            createParticle();
+        }
+
+
+        public void createParticle()
+        {
+            Vector2 startPosition = new Vector2(0.5f, 0.5f);
+            particle = new SplitterSystem(startPosition);
+        }
+
+
+        public void Draw(float elapsedTime)
+        {
+            durationTime += elapsedTime;
+            if(durationTime > 2)
+            {
+                createParticle();
+                durationTime = 0;
+            }
+
+            particle.Update(elapsedTime);
+
+            spriteBatch.Begin();
+
+            particle.Draw(spriteBatch, camera, particleTexture);
+
+            spriteBatch.End();
         }
 
 
