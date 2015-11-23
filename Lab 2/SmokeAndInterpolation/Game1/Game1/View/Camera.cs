11c2424e4698af85b81842ef08e544a0b8a766f5
ms223uh@student.lnu.se <ms223uh@student.lnu.se>
2015-11-23 14:31:03 +0100

    Done with cameraClass
    - getSmokeScale
    - getScreenCord
    - getRectangle

--- Lab 2/SmokeAndInterpolation/Game1/Game1/View/Camera.cs
+++ Lab 2/SmokeAndInterpolation/Game1/Game1/View/Camera.cs
@@ -10,5 +10,37 @@ namespace Game1.View
 {
     class Camera
     {
+
+        private float cordX;
+        private float cordY;
+
+        public Camera(Viewport viewPort)
+        {
+            cordX = viewPort.Width / 2;
+            cordY = viewPort.Height / 2;
+        }
+
+
+        public float getSmokeScale(float radius, int smokeWidth)
+        {
+            return (radius * 2.0f) * (float)cordX / (float)smokeWidth;
+        }
+
+
+        public Vector2 getScreenCord(Vector2 screenPosition)
+        {
+            int screenX = (int)((screenPosition.X * cordX));
+            int screenY = (int)((screenPosition.Y * cordY));
+            return new Vector2(screenX, screenY);
+        }
+
+        public Rectangle getRectangle(float x, float y)
+        {
+            return new Rectangle((int)(cordX * x), (int)(cordY * y), 64, 64);
+        }
+
+
+
+
     }
 }
