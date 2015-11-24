b285810c3e1788319ff41a10963b4fef034c958b
ms223uh@student.lnu.se <ms223uh@student.lnu.se>
2015-11-24 17:16:30 +0100

    Update Smokesystem
    - Added field and functions
    - Added comments

--- Lab 2/SmokeAndInterpolation/Game1/Game1/View/SmokeSystem.cs
+++ Lab 2/SmokeAndInterpolation/Game1/Game1/View/SmokeSystem.cs
@@ -10,6 +10,55 @@ namespace Game1.View
 {
     class SmokeSystem
     {
-        
+        Texture2D smokeTexture;
+        public int maxSmokeParticle;
+        float spawnNextParticle;
+        float lastParticle;
+        LinkedList<SmokeParticle> activeSmokeParticles;
+        Random random;
+
+        public SmokeSystem()
+        {
+            activeSmokeParticles = new LinkedList<SmokeParticle>();
+        }
+
+
+        public void Update(float elapsedTime)
+        {
+            LinkedListNode<SmokeParticle> smokeNode = activeSmokeParticles.First;
+
+            while(smokeNode != null)
+            {
+                bool particleIsAlive = smokeNode.Value.Update(elapsedTime);
+                smokeNode = smokeNode.Next;
+                if (!particleIsAlive)
+                {
+                    if (smokeNode == null)
+                    {
+                        activeSmokeParticles.RemoveLast();
+                    }
+                    else
+                    {
+                        activeSmokeParticles.Remove(smokeNode.Previous);
+                    }
+                }
+            }
+
+        }
+
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            LinkedListNode<SmokeParticle> smokeNode = activeSmokeParticles.First;
+            while (smokeNode != null)
+            {
+                smokeNode.Value.Draw(spritebatch, scale, offsett);
+                smokeNode = smokeNode.Next;
+            }
+        }
+
+
     }
+
+}
 
